f877aa9efe9b99a03e29cdda2cc0c0fd101c4316
bala-amypo <bala@amypo.in>
2025-12-17 05:28:18 +0000

    User commit

--- src/main/java/com/example/demo/Entity/StudentEntity.java
+++ src/main/java/com/example/demo/Entity/StudentEntity.java
@@ -15,7 +15,27 @@ public class StudentEntity{
     private String father_name;
     private int age;
 
+    public long getId(){
+        return studentId;
+    }
+
     public void setName(String name){
         this.name = name;
     }
+    public void setName(String father_name ){
+        this.father_name = father_name;
+    }
+    public void setName(int age){
+        this.age = age;
+    }
+     public String getName(){
+        return name;
+     }
+
+     public String getfather_name(){
+        return father_name;
+     }
+     public int getAge(){
+        return age;
+     }
 }
