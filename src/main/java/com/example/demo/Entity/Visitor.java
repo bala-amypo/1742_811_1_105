29767127954bf48e0764d01f9c3fbca1472316bd
bala-amypo <bala@amypo.in>
2025-12-21 05:31:36 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Visitor.java
+++ src/main/java/com/example/demo/Entity/Visitor.java
@@ -2,9 +2,16 @@ package .com.example.demo.Entity
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="Visitor")
+@Table(name="Visitor",uniqueConstraints={
+    @UniqueConsraint
+})
 
 public class Visitor{
     public Visitor(){}
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private String email;
+    private
 }
