f8e0c70e1c39fca1b1e072d6acb3d015067b8f18
bala-amypo <bala@amypo.in>
2025-12-21 05:28:37 +0000

    User commit

---
+++ src/main/java/com/example/demo/Entity/User.java
@@ -0,0 +1,5 @@
+package .com.example.demo.Entity
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="User")
---
+++ src/main/java/com/example/demo/Entity/Visitor.java
@@ -0,0 +1,10 @@
+package .com.example.demo.Entity
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Visitor")
+
+public class Visitor{
+    public Visitor(){}
+    
+}
