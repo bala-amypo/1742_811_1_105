439c008a90eaf44be5950cee79e08ce403493806
bala-amypo <bala@amypo.in>
2025-12-21 05:49:36 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Host.java
+++ src/main/java/com/example/demo/Entity/Host.java
@@ -1,5 +1,6 @@
-package .com.example.demo.Entity
+package .com.example.demo.Entity;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(
@@ -14,12 +15,61 @@ public class Host{
     public Host(){}
 
     @Id
-    @GeneratedValue(strategy=GeneratedType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String hostName;
     private String fullname;
     @Column(nullable=false)
     private String email;
     private String department;
-    
+    private LocalDateTime createdAt;
+
+     public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
--- src/main/java/com/example/demo/Entity/Visitor.java
+++ src/main/java/com/example/demo/Entity/Visitor.java
@@ -1,4 +1,4 @@
-package .com.example.demo.Entity
+package .com.example.demo.Entity;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
