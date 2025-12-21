bd6d47baeafc907ee8a02761ea68700b7b3f79f9
bala-amypo <bala@amypo.in>
2025-12-21 05:43:38 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Host.java
+++ src/main/java/com/example/demo/Entity/Host.java
@@ -1,2 +1,17 @@
 package .com.example.demo.Entity
 import jakarta.persistence.*;
+
+@Entity
+@Table(
+    name = "Host",
+    uniqueConstraints={
+        @UniqueConstraint(
+            columnNames={"email"}
+        )
+    }
+)
+public class Host{
+    public Host(){}
+
+    
+}
