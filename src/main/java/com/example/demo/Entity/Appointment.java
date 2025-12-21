3511996b565910b412f8f06cf37d35de24abb40f
bala-amypo <bala@amypo.in>
2025-12-21 05:55:35 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Appointment.java
+++ src/main/java/com/example/demo/Entity/Appointment.java
@@ -9,6 +9,13 @@ public class Appointment{
     public Appointment(){}
     public class ENUM{
         SCHEDULED,
-
-    }
+        CANCELLED,
+        COMPLETED
+        }
+    @Id
+    @GeneratedType(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinCloumn(name = "visitor_id")
+    private Visitor visitor
 }
