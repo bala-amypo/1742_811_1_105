f9966bfdd7b12621c739e19470a422c84a5178f2
bala-amypo <bala@amypo.in>
2025-12-21 06:13:36 +0000

    User commit

---
+++ src/main/java/com/example/demo/Entity/AlertNotification.java
@@ -0,0 +1,11 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="AlertNotification")
+public class AlertNotification{
+    public AlertNotification(){}
+    
+}
