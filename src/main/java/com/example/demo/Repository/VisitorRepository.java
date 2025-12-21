d64c025e09aaa54b81b07724e504e02f894da2df
bala-amypo <bala@amypo.in>
2025-12-21 09:07:36 +0000

    User commit

--- src/main/java/com/example/demo/Repository/VisitorRepository.java
+++ src/main/java/com/example/demo/Repository/VisitorRepository.java
@@ -1,2 +1,9 @@
-package .com.example.demo.Repository;
-import .com.example
+package com.example.demo.Repository;
+import com.example.demo.Entity.Visitor;
+import java.util.Optional;      
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface VisitorRepository extends JpaRepository<Visitor,Long>{
+    
+    Optional<Visitor> findByEmail(String email);
+    Optional<Visitor> findByPhone(String phone)
+}
