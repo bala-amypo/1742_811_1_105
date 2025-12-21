7a21fe7a58fcd5b4968f332f3f69d556ca8a28b0
bala-amypo <bala@amypo.in>
2025-12-21 06:01:35 +0000

    User commit

---
+++ src/main/java/com/example/demo/Entity/VisitLog.java
@@ -0,0 +1,11 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "VisitLog")
+public class VisitLog{
+    public VisitLog(){}
+    
+}
