0c0ea3e1fb71f914ff353842dae840a040c347c8
bala-amypo <bala@amypo.in>
2025-12-21 05:58:31 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Appointment.java
+++ src/main/java/com/example/demo/Entity/Appointment.java
@@ -5,17 +5,85 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "Appointment")
-public class Appointment{
-    public Appointment(){}
-    public class ENUM{
+public class Appointment {
+
+    public Appointment() {}
+
+    // ===== ENUM =====
+    public enum Status {
         SCHEDULED,
         CANCELLED,
         COMPLETED
-        }
+    }
+
+    
+
     @Id
-    @GeneratedType(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
-    @JoinCloumn(name = "visitor_id")
-    private Visitor visitor
+    @JoinColumn(name = "visitor_id", nullable = false)
+    private Visitor visitor;
+
+    @ManyToOne
+    @JoinColumn(name = "host_id", nullable = false)
+    private Host host;
+
+    private String purpose;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private LocalDateTime createdAt;
+
+  
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Visitor getVisitor() {
+        return visitor;
+    }
+
+    public void setVisitor(Visitor visitor) {
+        this.visitor = visitor;
+    }
+
+    public Host getHost() {
+        return host;
+    }
+
+    public void setHost(Host host) {
+        this.host = host;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
